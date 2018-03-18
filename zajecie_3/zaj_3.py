ff228acc95ac8357331594d7722036a4ceeb89e4
VladTrach <30550311+VladTrach@users.noreply.github.com>
2018-03-18 02:24:38 +0100

    Update zaj_3.py

--- zajecie_3/zaj_3.py
+++ zajecie_3/zaj_3.py
@@ -1,10 +1,71 @@
-trach.vladyslav@gmail.com#############################################
-
 from math import sqrt
 
+## zad 3 #################################
+
+class Silnik:
+  
+  def __init__(self):
+    self.__pojemnosc = 3.2
+    self.__uruchomiony = False
+    
+  def wlaczS(self):
+    self.__uruchomiony = True
+    
+  def wylaczS(self):
+    self.__uruchomiony = False
+  
+class Reflektor:
+  def __init__(self):
+    self.__sprawny = True
+    self.__swieci = False
+    
+  def wlaczR(self):
+    self.__swieci = True
+      
+  def wylaczR(self):
+    self.__swieci = False
+      
+  def czy_sprawny(self):
+    return self.__sprawny
+  
+class Hamulec:
+  def __init__(self):
+    self.__sprawny = True
+    self.__wcisniety = False
+    
+  def hamuj(self):
+    self.__wcisniety = True
+    
+  def nie_hamuj(self):
+    self.__wcisniety = False
+    
+  def czy_sprawny(self):
+    return self.__sprawny
+
+class Samochod(Hamulec,Reflektor,Silnik):
+  def __init__(self):
+    __kolor = "zuwty"
+    __numer_rejestracyjny = "student_UMCS"
+    
+    Hamulec.__init__(self)
+    Silnik.__init__(self)
+    Reflektor.__init__(self)
+  
+  def jedz(self):
+    self.wlaczS()
+    self.wlaczR()
+    
+  def hamujSamochod(self):
+    self.hamuj()
+
+  
+    
+
+## zad 2 #################################
+
 class Punkt2D:
-  x = 0
-  y = 0
+  x = None
+  y = None
   
   def __init__(self,x,y):
     self.x = x
@@ -14,7 +75,7 @@ class Punkt2D:
     return sqrt( pow(self.x - other.x,2) + pow(self.y - other.y,2))
     
 class Punkt3D(Punkt2D):
-  z = 0
+  z = None
     
   def __init__(self,x,y,z):
     super(Punkt3D,self).__init__( x, y)
@@ -27,10 +88,14 @@ p2 = Punkt3D(1,2,1)
 p3 = Punkt3D(1,6,1)
 
 print(p2.oblicz(p3))
-print()
-########################################
+
+
+## zad 1 #########################################
 
 class LiczbaZespolona:
+  rzecz = None
+  uroj = None
+  
   def __init__(self,rzecz,uroj):
     self.rzecz = rzecz
     self.uroj = uroj
@@ -54,8 +119,38 @@ class LiczbaZespolona:
     b2 = (self.uroj * other.rzecz - self.rzecz * other.uroj) / (other.rzecz**2 + other.uroj**2)
     
     return LiczbaZespolona(a2,b2)
-  
     
+  def __str__(self):
+    return str(self.rzecz)+" + "+str(self.uroj)+"i"
+    
+  def __setattr__(self,name,value):
+    return object.__setattr__(self, name, value)
+    
+  def modul(self):
+    return sqrt(self.rzecz**2 + self.uroj**2)
+    
+  def __lt__(self,other):
+    return self.modul() < other.modul()
+
+  def __le__(self,other):
+    return self.modul() <= other.modul()
+
+  def __eq__(self,other):
+    return self.modul() == other.modul()
+    
+  def __ne__(self,other):
+    return self.modul() != other.modul()
+    
+  def __gt__(self,other):
+    return self.modul() > other.modul()
+    
+  def __ge__(self,other):
+    return self.modul() >= other.modul()
+
+liczbaZ = LiczbaZespolona(5,1)
+liczbaZ2 = LiczbaZespolona(5,1)
+
+print(liczbaZ2 >= liczbaZ )    
 
 ########################################
 
@@ -119,3 +214,4 @@ obiekt.atr1 = 5
 obiekt.setArt2(8)
 obiekt.setAtr3(11)
 print(obiekt.add())
+
